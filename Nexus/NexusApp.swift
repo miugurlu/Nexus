ea63cb1f28755a9108beeb562642eef125ba5767
miugurlu <miugurlu@icloud.com>
2025-05-28 14:26:27 +0300

    Initial Commit

---
+++ Nexus/NexusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexusApp.swift
+//  Nexus
+//
+//  Created by İbrahim Uğurlu on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct NexusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NexusTests/NexusTests.swift
@@ -0,0 +1,17 @@
+//
+//  NexusTests.swift
+//  NexusTests
+//
+//  Created by İbrahim Uğurlu on 28.05.2025.
+//
+
+import Testing
+@testable import Nexus
+
+struct NexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
